a70ae1da100ae1d0102eedbb15c6f287c78482a6
jean-francois fays <fays.jf@gmail.com>
2023-07-27 22:20:22 +0200

    contact form in progress

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,24 @@
+class ContactsController < ApplicationController
+  # require 'mail_form'
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contacts_params)
+    @contact.request = request
+    if @contact.deliver
+      redirect_to new_contact_path, notice: "Message envoyé!"
+    else
+      # flash.now[:error] = "Impossible d'envoyer le message, veuillez remplir tous les champs requis!"
+      redirect_to new_contact_path, notice: "Impossible d'envoyer le message, veuillez remplir tous les champs requis!"
+      # render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def contacts_params
+    params.require(:contact).permit(:first_name, :last_name, :phone_number, :city, :email, :message)
+  end
+end
