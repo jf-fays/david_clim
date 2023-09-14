ca5c0a31eb1dab2804d8ba0c5ddb4517730e8c8f
jean-francois fays <fays.jf@gmail.com>
2023-09-14 15:50:02 +0200

    text installation page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "contact", to: "pages#contact_form"
+  # get "contact", to: "pages#contact_form"
+  get "installation-climatisation", to: "pages#installation"
+  get "maintenance-nettoyage-climatisation", to: "pages#maintenance_nettoyage"
+  get "depannage-climatisation", to: "pages#depannage"
+  get "climatisation-reversible-pompe-a-chaleur", to: "pages#clim_pompe_a_chaleur"
+  get "realisation-climatisation", to: "pages#realisation"
+
 
   resources :contacts, only: [:new, :create ]
     # get '/contacts', to: 'contacts#new'
