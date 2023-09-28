87c44646eea9ce643cbb398571bf0551c448a614
jean-francois fays <fays.jf@gmail.com>
2023-09-28 18:56:39 +0200

    carousel spinner

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,16 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+
+// require ("jquery")
+// require ("jquery_ujs")
+// require ("turbolinks")
+// // require_tree .")
+// require ("carousel.js")
+// import "carousel"
+
+// console.log('Hello world3');
+import { carousel } from "./components/carousel";
+
+carousel()
