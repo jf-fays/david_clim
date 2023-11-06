73098b6486edc44520e1dac3ba0005d47786671c
jean-francois fays <fays.jf@gmail.com>
2023-11-06 12:32:41 +0100

    test 2

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -12,7 +12,7 @@ import "bootstrap"
 // import "carousel"
 
 // console.log('Hello world3');
-import { initCarousel } from "./components/carousel";
-document.addEventListener("turbo:load", () => {
+import { initCarousel } from "./controllers/carousel";
+document.addEventListener("turbo:reload", () => {
   initCarousel();
 });
